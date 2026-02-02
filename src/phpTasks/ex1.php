182e56d7a0105484c9eccb236e70b209b17df37b
Akmeemana Madduma Acharige Maheessha Nuwangie Sumanadasa <amk1005932@student.hamk.fi>
2026-02-02 15:16:50 +0200

    first commit

---
+++ src/phpTasks/ex1.php
@@ -0,0 +1,22 @@
+<html>
+    <body>
+<?php
+echo "<h4>PHP is interesting.</h4>";
+echo "<h3>Hello world! My name is 'David'</h3>"; 
+$g1 = 5;
+$g2 = 4;
+$g3 = 5;
+
+echo "<table border='1'>"; 
+echo "<tr>
+<th>S.n</th>
+<th>name</th>
+<th>grade</th>
+</tr>";
+echo "<tr><td>1</td><td>John</td><td>$g1</td></tr>";
+echo "<tr><td>2</td><td>Alice</td><td>$g2</td></tr>";
+echo "<tr><td>3</td><td>Bob</td><td>$g3</td></tr>";
+echo "</table>";
+?>
+</body>
+</html>
